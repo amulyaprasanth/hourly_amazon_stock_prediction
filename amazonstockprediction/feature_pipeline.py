100cee2ffc814e37d203be53cd531347dbe7e932
amulyaprasanth <amulyaprasanth301@gmail.com>
2025-03-06 15:33:36 +0530

    fixed project structure

--- amazonstockprediction/feature_pipeline.py
+++ amazonstockprediction/feature_pipeline.py
@@ -1,8 +1,8 @@
 import os
 import hopsworks
-from amazonstockprediction.src.logger import setup_logger
+from amazonstockprediction.logger import setup_logger
 from dotenv import load_dotenv
-from amazonstockprediction.src.utils import fetch_historical_data, calculate_indicators
+from amazonstockprediction.utils import fetch_historical_data, calculate_indicators
 
 load_dotenv()
 
