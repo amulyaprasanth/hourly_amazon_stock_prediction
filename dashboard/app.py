2db5ab62a503a8249d684883927e897245d9ae67
amulyaprasanth <amulyaprasanth301@gmail.com>
2025-03-09 09:55:15 +0530

    updated dashboard to update dates for predictions

--- dashboard/app.py
+++ dashboard/app.py
@@ -103,23 +103,8 @@ for i, col in enumerate(cols):
     with col:
         last_value = st.session_state.predictions.iloc[-(i+1)].close
         st.metric(
-            f"Prediction {i+1}",
+            f"{st.session_state.predictions.iloc[-(i+1)].id}",
             value=f"${last_value:.2f}",
         )
 plot_predictions()
 plot_candle_chart(st.session_state["data"])
-
-
-# Disclaimer
-st.markdown("---")
-st.markdown(
-    """
-    **Disclaimer:**  
-    This application is for educational purposes only and is not intended to provide financial or investment advice. 
-    The stock predictions are based on historical data and algorithms, which may not predict future market behavior. 
-    Users should not rely on this application for trading decisions. Always consult with a qualified financial advisor 
-    before making any investment choices. The creators are not responsible for any financial losses or damages resulting 
-    from the use of this app.
-    """
-)
-
